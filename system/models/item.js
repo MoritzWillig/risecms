dfce6398ec745e0d948fcb2df9fc753b235d6915
Moritz Willig <moritz.willig@gmail.com>
2014-09-10 18:14:10 +0200

    added documentation to item and itemLink object

--- system/models/item.js
+++ system/models/item.js
@@ -7,12 +7,17 @@ http = require('http');
 /**
  * checks if the given parameter can be interpreted as a number
  * @param  {*} x parameter to be checked
- * @return {boolean}   wheter or not the parameter is a number
+ * @return {boolean}   whether or not the parameter is a number
  */
 function checkNumber(x) {
   return !isNaN(+x);
 }
 
+/**
+ * Item data representation
+ * @param {string/integer} id     id,name or path of the item
+ * @param {boolean} asPath if true the id is interpreted as path
+ */
 function Item(id,asPath) {
   if (id!=undefined) {
     this.id=id;
@@ -24,39 +29,104 @@ function Item(id,asPath) {
 
   this.statusHeader=new stat.states.items.NOT_LOADED();
   this.statusFile=new stat.states.items.NOT_LOADED();
+
+  this.itemStr=[];
 }
 
+/**
+ * id of the item
+ * @type {[number/string]}
+ */
 Item.prototype.id=undefined;
+/**
+ * describes the way the item id has to be interpreted
+ * "id": item id
+ * "name": item name
+ * "string": item does not have a database id
+ * @type {string}
+ */
 Item.prototype.idType=undefined;
 
+/**
+ * status of the item header loaded from database
+ * @type {Status}
+ */
 Item.prototype.statusHeader=undefined;
+/**
+ * status of the item file & item interpretation
+ * @type {Status}
+ */
 Item.prototype.statusFile=undefined;
 
-
+/**
+ * database row if item was loaded from database
+ * @type {object/null}
+ */
 Item.prototype.header=null;
+/**
+ * Item string of the item
+ * @type {[string]}
+ */
 Item.prototype.file=undefined;
-Item.prototype.dataScope={};
+//Item.prototype.dataScope={};
 
-Item.prototype.itemStr=[]; //[string/Item]
-Item.prototype.dataObj=undefined; //object
-Item.prototype.script=undefined; //Script object
+/**
+ * parsed file attribute. Item elements have to be parsed and the whole array concernated to get the final item string
+ * @type {[array/string]}
+ */
+Item.prototype.itemStr=[];
+/**
+ * object resulting from parsed file attribute
+ * @type {[object]}
+ */
+Item.prototype.dataObj=undefined;
+/**
+ * script object created from the filev attribute
+ * @type {[Script]}
+ */
+Item.prototype.script=undefined;
 
-Item.prototype.parent=undefined; //parent including this item
-Item.prototype.staticParent=undefined; //static parent from database
-Item.prototype.modifier={post:[]};
+/**
+ * dynamic parent holding this item (-> "including item")
+ * @type {[Item]}
+ */
+Item.prototype.parent=undefined;
+/**
+ * static parent of the item
+ * @type {[Item]}
+ */
+Item.prototype.staticParent=undefined;
+//Item.prototype.modifier={post:[]};
 
+/**
+ * checks if the item is in an valid state
+ * @return {boolean} returns true if item is valid
+ */
 Item.prototype.isValid=function() {
   return !(this.hasHeaderErr() || this.hasFileErr());
 }
 
+/**
+ * checks if the item header is in an valid state
+ * @return {boolean} returns false if item header is valid
+ */
 Item.prototype.hasHeaderErr=function() {
   return !(this.statusHeader instanceof status.proto.item.valid);
 }
 
+/**
+ * checks if the item file is in an valid state
+ * @return {boolean} returns false if item file is valid
+ */
 Item.prototype.hasFileErr=function() {
   return !(this.statusFile instanceof status.proto.item.valid);
 }
 
+/**
+ * returns a status string of the item
+ * @param  {boolean} printValid adds status of valid states
+ * @return {string}            status string of the item
+ */
 Item.prototype.getStatusString=function(printValid) {
   var str="";
   if ((printValid) || (this.hasHeaderErr())) {
@@ -70,6 +140,11 @@ Item.prototype.getStatusString=function(printValid) {
   return str;
 }
 
+/**
+ * loads the header of the item from the database based on item id and idType
+ * @param  {function} (Item self) callback if the header was loaded
+ * @async
+ */
 Item.prototype.loadHeader=function(callback) {
   this.header=undefined;
 
@@ -108,7 +183,8 @@ Item.prototype.loadHeader=function(callback) {
 /**
  * queries the item header by id
  * @private
- * @param  {Function} callback callback to return the result
+ * @param  {Function} callback (String err, Object data) callback to return the result
+ * @async
  */
 Item.prototype._fromId=function(callback) {
   var query=db.query('SELECT * FROM ?? WHERE id=?',[cg.database.pageTable,this.id],callback);
@@ -117,7 +193,8 @@ Item.prototype._fromId=function(callback) {
 /**
  * queries the item header by name
  * @private
- * @param  {Function} callback callback to return the result
+ * @param  {Function} callback (String err, Object data) callback to return the result
+ * @async
  */
 Item.prototype._fromName=function(callback) {
   var query=db.query('SELECT * FROM ?? WHERE name=?',[cg.database.pageTable,this.id],callback);
@@ -126,25 +203,36 @@ Item.prototype._fromName=function(callback) {
 /**
  * queries the item header by path
  * @private
- * @param  {Function} callback callback to return the result
+ * @param  {Function} callback (String err, Object data) callback to return the result
+ * @async
  */
 Item.prototype._fromPath=function(callback) {
   var query=db.query('SELECT * FROM ?? WHERE path=?',[cg.database.pageTable,this.id],callback);
 }
 
+/**
+ * returns the the item file path
+ * @return {string} path to item file
+ * @throws {INVALID_ID} If no id was set or loaded from the database
+ */
 Item.prototype.resolveIdPath=function() {
   var id=this.id;
   if (this.idType!="id") {
     if (this.header!=undefined) {
       id=this.header.id;
     } else {
-      throw new stat.states.item.INVALID_ID({id:this.id,type:this.idType,this:this});
+      throw new stat.states.item.INVALID_ID({id:this.id,type:this.idType});
     }
   }
 
   return fsanchor.resolve(id,"storage");
 }
 
+/**
+ * loads the file of the item
+ * @param  {function} callback (Item self) callback if the item is loaded
+ * @async
+ */
 Item.prototype.loadFile=function(callback) {
   this.file=undefined;
 
@@ -162,6 +250,15 @@ Item.prototype.loadFile=function(callback) {
   });
 }
 
+/**
+ * initiates and item from a string
+ * @param  {string}   str      item string to be set
+ * @param  {string}   type     item string
+ * @param  {string}   name     item name
+ * @param  {function} callback (Item self) callback to be called if item is initiated
+ * @sync
+ * @async
+ */
 Item.prototype.loadString=function(str,type,name,callback) {
   this.id=name;
   this.header={type:type};
--- system/models/itemLink.js
+++ system/models/itemLink.js
@@ -1,20 +1,44 @@
 
 Item=require("./item.js");
 
+/**
+ * ItemLink holding environment data of items
+ * @param {Item/ItemLink} item      item to be linked to or itemlink to be copied
+ * @param {[{}/Item]} data      additional environment data (or item holding additional data)
+ * @param {[*]} modifiers modifiers to apply at item parsing
+ */
 function ItemLink(item,data,modifiers) {
-  this.item=item;
-  
-  this.setData(data);
+  if (item instanceof ItemLink) {
+    //copy
+    this.item=item.item;
+    this.data=item.data;
+    this.modifiers=item.modifiers;
+    this.dataItem=item.dataItem;
+    this.replacements=item.replacements;
+  } else {
+    this.item=item;
+    
+    this.setData(data);
 
-  if (typeof modifiers!="undefined") {
-    this.modifiers=modifiers;
+    if (typeof modifiers!="undefined") {
+      this.modifiers=modifiers;
+    }
   }
 }
 
+/**
+ * checks if the item link can be parsed
+ * @return {boolean} returns true if the itemlink is in a valid state
+ */
 ItemLink.prototype.isValid=function() {
   return ((this.item.isValid()) && ((this.dataItem==undefined) || (this.dataItem.isValid())));
 }
 
+/**
+ * returns a status string of the item link
+ * @param  {boolean} printValid adds status messages of valid items
+ * @return {string}            status string of the item link
+ */
 ItemLink.prototype.getStatusString=function(printValid) {
   var str="";
   if ((!this.item.isValid()) || printValid) {
@@ -34,19 +58,49 @@ ItemLink.prototype.getStatusString=function(printValid) {
   return str;
 }
 
+/**
+ * sets additional data
+ * @param {{}/Item} data data or "data "item holding the data
+ */
 ItemLink.prototype.setData=function(data) {
-  if (data instanceof Item) {
-    this.data=data.dataObj;
-    this.dataItem=data;
+  if (data==undefined) {
+    this.data={};
   } else {
-    this.data=(typeof data != "undefined")?data:{};
-    this.dataItem=undefined;
+    if (data instanceof Item) {
+      this.data=data.dataObj;
+      this.dataItem=data;
+    } else {
+      this.data=(typeof data != "undefined")?data:{};
+      this.dataItem=undefined;
+    }
   }
 }
 
+/**
+ * linked item
+ * @type {Item}
+ */
 ItemLink.prototype.item=undefined;
+/**
+ * additional environment data
+ * @type {Object}
+ */
 ItemLink.prototype.data={};
-ItemLink.prototype.dataItem=undefined; //set if data was loaded from an item
+/**
+ * item which provided the additional data
+ * @type {[Object]}
+ */
+ItemLink.prototype.dataItem=undefined;
+/**
+ * modifiers to apply on item parsing
+ * @type {Object}
+ * @todo
+ */
 ItemLink.prototype.modifiers={post:[]};
+/**
+ * replacements to apply to the data of the linked item
+ * @type {Array}
+ */
+ItemLink.prototype.replacements=[];
 
 module.exports=ItemLink;
